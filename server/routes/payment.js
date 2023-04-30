bd42d5d2a69fe27a4839a0417a2166bcc4a341f7
Satwik-pal-git <satwikpal01@gmail.com>
2023-04-30 16:53:29 +0530

    payment files added

---
+++ server/routes/payment.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const PaymentUtils = require("../utils/payPal");
+
+const router = express.Router();
+
+router
+    .route("/payment")
+    .post(PaymentUtils.Payment);
+
+router
+    .route("/cancel")
+    .get(PaymentUtils.Cancelled);
+
+module.exports = router;
